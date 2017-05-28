12d85a72d2dba2f45817f39e4c8672f146db6250
m.ewert <github@markus-ewert.de>
2017-05-28 19:50:49 +0200

    Added first version of a second screen.

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ var shiftindicator = 0.985;
 var ws = null;
 
 $(document).ready(function () {
+	
+	window.onclick = function() { switchScreen(); };
+	
 	createRpmBarMarkers();
 	wakeUpServer();
 	setInterval(function () { wakeUpServer(); }, 3000);
 });
+
+function switchScreen() {
+	$('#first_screen').toggle();
+	$('#second_screen').toggle();
+	
+	//if($('first_screen').css('display') == 'none');
+}
 
 function wakeUpServer() {
 	if (ws == null || (ws != undefined && ws.readyState != ws.OPEN)) {
@@ -46,10 +56,6 @@ function startClient() {
 }
 
 function update(json) {
-	if (json.MaxEngineRpm == 0) {
-		return;
-	}
-	
 	var gear = parseFloat(json.Gear);
 	if (gear == 0) {
 		gear = 'N';
@@ -62,8 +68,8 @@ function update(json) {
 		return;
 	}
 	
-	document.getElementById("speed").innerHTML = json.CarSpeed.toFixed(0);
-	document.getElementById("gear").innerHTML = gear;
+	$(".speed").html(json.CarSpeed.toFixed(0));
+	$(".gear").html(gear);
 	
 	// RPM stuff.	
 	if (json.MaxEngineRpm > 0) {
@@ -73,38 +79,36 @@ function update(json) {
 	}
 	var rpm_per = json.EngineRpm/MAXRPM;
 	
-	//document.getElementById("rpm").innerHTML = json.EngineRps.toFixed(0);
-	document.getElementById("rpm_bar").style.width = convertRpmToPercent(rpm_per) + "%";
+	//$("#rpm").html(json.EngineRps.toFixed(0);
+	$("#rpm_bar").width(convertRpmToPercent(rpm_per) + "%");
 	
-	document.getElementById("led1").className = rpm_per >= 0.500 ? "green" : "";
-	document.getElementById("led2").className = rpm_per >= 0.575 ? "green" : "";
-	document.getElementById("led3").className = rpm_per >= 0.650 ? "green" : "";
-	document.getElementById("led4").className = rpm_per >= 0.725 ? "green" : "";
+	$("#led1").className = rpm_per >= 0.500 ? "green" : "";
+	$("#led2").className = rpm_per >= 0.575 ? "green" : "";
+	$("#led3").className = rpm_per >= 0.650 ? "green" : "";
+	$("#led4").className = rpm_per >= 0.725 ? "green" : "";
 	
-	document.getElementById("led5").className = rpm_per >= 0.80 ? "yellow" : "";
-	document.getElementById("led6").className = rpm_per >= 0.83 ? "yellow" : "";
-	document.getElementById("led7").className = rpm_per >= 0.86 ? "yellow" : "";
-	document.getElementById("led8").className = rpm_per >= 0.89 ? "yellow" : "";
+	$("#led5").className = rpm_per >= 0.80 ? "yellow" : "";
+	$("#led6").className = rpm_per >= 0.83 ? "yellow" : "";
+	$("#led7").className = rpm_per >= 0.86 ? "yellow" : "";
+	$("#led8").className = rpm_per >= 0.89 ? "yellow" : "";
 	
-	document.getElementById("led9").className  = rpm_per >= 0.91 ? "red" : "";
-	document.getElementById("led10").className = rpm_per >= 0.93 ? "red" : "";
-	document.getElementById("led11").className = rpm_per >= 0.95 ? "red" : "";
-	document.getElementById("led12").className = rpm_per >= 0.97 ? "red" : "";
+	$("#led9").className  = rpm_per >= 0.91 ? "red" : "";
+	$("#led10").className = rpm_per >= 0.93 ? "red" : "";
+	$("#led11").className = rpm_per >= 0.95 ? "red" : "";
+	$("#led12").className = rpm_per >= 0.97 ? "red" : "";
 	
 	// Special styles.
-	document.getElementById("leds").className = rpm_per >= shiftindicator || json.PitLimiter ? "blink" : "";
-	document.getElementById("gear").className = rpm_per >= shiftindicator ? "shift_indicator blink" : "";
-	document.getElementById("speed").className = json.DrsEngaged ? "speed_drs_active blink" : "";
+	$("#leds").className = rpm_per >= shiftindicator || json.PitLimiter ? "blink" : "";
+	$(".gear").className = rpm_per >= shiftindicator ? "shift_indicator blink" : "";
+	$("#speed").className = json.DrsEngaged ? "speed_drs_active blink" : "";
 	
 	// Fuel.
-	document.getElementById("fuellaps").innerHTML = json.FuelLapsLeftEstimate > 0 ? (Math.floor(json.FuelLapsLeftEstimate * 10) / 10).toFixed(1) : "-";
-	document.getElementById("fuel").innerHTML = (Math.floor(json.FuelLeft * 10) / 10).toFixed(1);
-	document.getElementById("avgfuel").innerHTML = json.FuelPerLap > 0 ? (Math.ceil(json.FuelPerLap * 10) / 10).toFixed(1) : "-";
+	$("#fuellaps").html(json.FuelLapsLeftEstimate > 0 ? (Math.floor(json.FuelLapsLeftEstimate * 10) / 10).toFixed(1) : "-");
+	$("#fuel").html((Math.floor(json.FuelLeft * 10) / 10).toFixed(1));
+	$("#avgfuel").html(json.FuelPerLap > 0 ? (Math.ceil(json.FuelPerLap * 10) / 10).toFixed(1) : "-");
 	
-	document.getElementById("lap").innerHTML = (json.NumberOfLaps > 0 && json.CompletedLaps >= json.NumberOfLaps ? json.NumberOfLaps : json.CompletedLaps + 1) + (json.NumberOfLaps > 0 ? "/" + json.NumberOfLaps : "");
-	document.getElementById("pos").innerHTML = json.Position + "/" + json.NumCars;
-	
-	document.getElementById("pitlimiter").innerHTML = json.PitLimiter == 1 ? 'yes' : 'no';
+	$("#lap").html((json.NumberOfLaps > 0 && json.CompletedLaps >= json.NumberOfLaps ? json.NumberOfLaps : json.CompletedLaps + 1) + (json.NumberOfLaps > 0 ? "/" + json.NumberOfLaps : ""));
+	$("#pos").html(json.Position + "/" + json.NumCars);
 	
 	// DRS
 	var drsText = '-';
@@ -124,8 +128,8 @@ function update(json) {
 		}
 	}
 
-	document.getElementById("drs").innerHTML = drsText;
-	document.getElementById("drs").className = drsStyle;
+	$("#drs").html(drsText);
+	$("#drs").className = drsStyle;
 	
 	//P2P
 	var p2pText = '-';
@@ -144,25 +148,25 @@ function update(json) {
 		}
 	}
 
-	document.getElementById("p2p").innerHTML = p2pText;
-	document.getElementById("p2p").className = p2pStyle;
+	$("#p2p").html(p2pText);
+	$("#p2p").className = p2pStyle;
 	
 	// Times.
-	document.getElementById("laptime").innerHTML = formatLapTime(json.LapTimeCurrentSelf);
-	document.getElementById("lastlap").innerHTML = formatLapTime(json.LapTimePreviousSelf);
-	document.getElementById("bestlap").innerHTML = formatLapTime(json.LapTimeBestSelf);
-	document.getElementById("sessiontime").innerHTML = formatTime(json.SessionTimeRemaining);
-	document.getElementById("clock").innerHTML = moment().local().format("HH:mm:ss");
+	$("#laptime").html(formatLapTime(json.LapTimeCurrentSelf));
+	$("#lastlap").html(formatLapTime(json.LapTimePreviousSelf));
+	$("#bestlap").html(formatLapTime(json.LapTimeBestSelf));
+	$("#sessiontime").html(formatTime(json.SessionTimeRemaining));
+	$("#clock").html(moment().local().format("HH:mm:ss"));
 	
 	//Delta
 	if (json.LapTimeCurrentSelf > 0 && json.LapTimeBestSelf > 0) {
 		var delta = json.DeltaBestSelf;
 		
-		document.getElementById("delta").innerHTML = (delta >= 0 ? '+' : '') + delta.toFixed(3);
-		document.getElementById("delta").className = delta >= 0 ? 'positive' : 'negative';
+		$("#delta").html((delta >= 0 ? '+' : '') + delta.toFixed(3));
+		$("#delta").className = delta >= 0 ? 'positive' : 'negative';
 	} else {
-		document.getElementById("delta").innerHTML = '-';
-		document.getElementById("delta").className = '';
+		$("#delta").html('-');
+		$("#delta").className = '';
 	}
 	
 	// Flags.
@@ -189,6 +193,22 @@ function update(json) {
 	if (flag.className != flagClass) {
 		flag.className = flagClass;
 	}
+	
+	// Tires.
+	$('#tire_temp_front_left').html(Math.floor(json.TireTempFrontLeft) + '°C');
+	$('#tire_temp_front_right').html(Math.floor(json.TireTempFrontRight) + '°C');
+	$('#tire_temp_rear_left').html(Math.floor(json.TireTempRearLeft) + '°C');
+	$('#tire_temp_rear_right').html(Math.floor(json.TireTempRearRight) + '°C');
+	
+	$('#tire_wear_front_left').html(Math.floor(json.TireWearFrontLeft) + '%');
+	$('#tire_wear_front_right').html(Math.floor(json.TireWearFrontRight) + '%');
+	$('#tire_wear_rear_left').html(Math.floor(json.TireWearRearLeft) + '%');
+	$('#tire_wear_rear_right').html(Math.floor(json.TireWearRearRight) + '%');
+	
+	$('#tire_pressure_front_left').html(Math.floor(json.TirePressureFrontLeft) + 'psi');
+	$('#tire_pressure_front_right').html(Math.floor(json.TirePressureFrontRight) + 'psi');
+	$('#tire_pressure_rear_left').html(Math.floor(json.TirePressureRearLeft) + 'psi');
+	$('#tire_pressure_rear_right').html(Math.floor(json.TirePressureRearRight) + 'psi');
 }
 
 function formatLapTime(sec) {
@@ -214,7 +234,7 @@ function updateMaxRpm(maxrpm_tmp, override) {
 }
 
 function createRpmBarMarkers() {
-	var rpmBar = document.getElementById("rpm_bar");
+	var rpmBar = $("#rpm_bar");
 	
 	// Remove old grid lines.
 	while (rpmBar.firstChild) {
@@ -226,6 +246,6 @@ function createRpmBarMarkers() {
 	for (var i = 0; i < percentages.length; i++) {
 		var line = document.createElement("div");
 		line.style.left = convertRpmToPercent(percentages[i] / 100).toFixed(2) + "%";
-		rpmBar.appendChild(line); 
+		rpmBar.append(line); 
 	}
 }
