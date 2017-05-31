74190254623a789d0d4f1e14fce55cf6e631d9ba
m.ewert <github@markus-ewert.de>
2017-05-31 21:37:45 +0200

    Added first version of a pit screen. Improved JS code.

--- script.js
+++ script.js
@@ -1,19 +1,132 @@
 var MAXRPM = 0;
-var shiftindicator = 0.925;
+var shiftindicator = 0.95;
 var ws = null;
 
+// Screens.
+var firstScreen;
+var secondScreen;
+var pitScreen;
+
+// Elements.
+var rpmBar;
+var rpmLeds;
+var fuelLeft;
+var lapTime;
+var lastLap;
+var bestLap;
+var sessionTime;
+var clock;
+var elGear;
+var elSpeed;
+var drs;
+var p2p;
+var timeInPitLane;
+var pitLimiter;
+var delta;
+var fuelLaps;
+var avgFuel;
+var lap;
+var pos;
+var flag;
+var tireTempFL;
+var tireTempFR;
+var tireTempRL;
+var tireTempRR;
+var tireWearFL;
+var tireWearFR;
+var tireWearRL;
+var tireWearRR;
+var tirePressureFL;
+var tirePressureFR;
+var tirePressureRL;
+var tirePressureRR;
+
 $(document).ready(function () {
+	initElements();
+	window.onclick = function() { switchScreen(false); };
 	
-	window.onclick = function() { switchScreen(); };
-	
-	createRpmBarMarkers();
 	wakeUpServer();
 	setInterval(function () { wakeUpServer(); }, 3000);
 });
 
-function switchScreen() {
-	$('#first_screen').toggle();
-	$('#second_screen').toggle();
+function initElements() {
+	firstScreen = $('#first_screen');
+	secondScreen = $('#second_screen')
+	pitScreen = $('#pit_screen');
+	
+	rpmLeds = $('#leds div');
+	elGear = $('.gear');
+	elSpeed = $('.speed');
+	
+	fuelLeft = $('.fuelleft');
+	fuelLaps = $('#fuellaps');
+	avgFuel = $('#avgfuel');
+	
+	lap = $('lap');
+	pos = $('pos');
+	
+	lapTime = $('#laptime');
+	lastLap = $('#lastlap');
+	bestLap = $('#bestlap');
+	sessionTime = $('#sessiontime');
+	clock = $('#clock');
+	
+	delta = $('#delta');
+	
+	drs = $('#drs');
+	p2p = $('#p2p');
+	
+	timeInPitLane = $('#time_in_pitlane');
+	pitLimiter = $('.pitlimiter div');
+	
+	flag = $('#flag');
+	
+	tireTempFL = $('#tire_temp_front_left');
+	tireTempFR = $('#tire_temp_front_right');
+	tireTempRL = $('#tire_temp_rear_left');
+	tireTempRR = $('#tire_temp_rear_right');
+	
+	tireWearFL = $('#tire_wear_front_left');
+	tireWearFR = $('#tire_wear_front_right');
+	tireWearRL = $('#tire_wear_rear_left');
+	tireWearRR = $('#tire_wear_rear_right');
+	
+	tirePressureFL = $('#tire_pressure_front_left');
+	tirePressureFR = $('#tire_pressure_front_right');
+	tirePressureRL = $('#tire_pressure_rear_left');
+	tirePressureRR = $('#tire_pressure_rear_right');
+	
+	// Init RPM bar.
+	rpmBar = $('#rpm_bar');
+	
+	// Remove old grid lines.
+	while (rpmBar.firstChild) {
+		rpmBar.removeChild(rpmBar.firstChild);
+	}
+	
+	// Add new grid lines.
+	var percentages = Array(15, 25, 50, 70, 80, 90, 95, 98);
+	for (var i = 0; i < percentages.length; i++) {
+		var line = document.createElement("div");
+		line.style.left = convertRpmToPercent(percentages[i] / 100).toFixed(2) + "%";
+		rpmBar.append(line); 
+	}
+}
+
+function switchScreen(inPit) {
+	if (inPit) {
+		firstScreen.hide();
+		secondScreen.hide();
+		pitScreen.show();
+	} else {
+		if (pitScreen.is(':visible')) {
+			firstScreen.show();
+			pitScreen.hide();
+		} else {
+			firstScreen.toggle();
+			secondScreen.toggle();
+		}
+	}
 	
 	//if($('first_screen').css('display') == 'none');
 }
@@ -68,8 +181,8 @@ function update(json) {
 		return;
 	}
 	
-	$(".speed").html(json.CarSpeed.toFixed(0));
-	$(".gear").html(gear);
+	elSpeed.html(json.CarSpeed.toFixed(0));
+	elGear.html(gear);
 	
 	// RPM stuff.	
 	if (json.MaxEngineRpm > 0) {
@@ -79,8 +192,7 @@ function update(json) {
 	}
 	var rpm_per = json.EngineRpm/MAXRPM;
 	
-	//$("#rpm").html(json.EngineRps.toFixed(0);
-	$("#rpm_bar").width(convertRpmToPercent(rpm_per) + "%");
+	rpmBar.width(convertRpmToPercent(rpm_per) + "%");
 	
 	$("#led1").toggleClass('green', rpm_per >= 0.500);
 	$("#led2").toggleClass('green', rpm_per >= 0.575);
@@ -98,20 +210,19 @@ function update(json) {
 	$("#led12").toggleClass('red', rpm_per >= 0.97);
 	
 	// Special styles.
-	$("#leds div").toggleClass('blink', rpm_per >= shiftindicator || json.PitLimiter == 1);
-	$(".gear").toggleClass('shift_indicator', rpm_per >= shiftindicator);
-	$(".speed").toggleClass('speed_drs_active', json.DrsEngaged > 0);
+	rpmLeds.toggleClass('blink', rpm_per >= shiftindicator || json.PitLimiter == 1);
+	elGear.toggleClass('shift_indicator', rpm_per >= shiftindicator);
+	elSpeed.toggleClass('speed_drs_active', json.DrsEngaged > 0);
 	
 	// Fuel.
-	$("#fuellaps").html(json.FuelLapsLeftEstimate > 0 ? (Math.floor(json.FuelLapsLeftEstimate * 10) / 10).toFixed(1) : "-");
-	$("#fuel").html((Math.floor(json.FuelLeft * 10) / 10).toFixed(1));
-	$("#avgfuel").html(json.FuelPerLap > 0 ? (Math.ceil(json.FuelPerLap * 10) / 10).toFixed(1) : "-");
+	fuelLaps.html(json.FuelLapsLeftEstimate > 0 ? (Math.floor(json.FuelLapsLeftEstimate * 10) / 10).toFixed(1) : "-");
+	fuelLeft.html((Math.floor(json.FuelLeft * 10) / 10).toFixed(1));
+	avgFuel.html(json.FuelPerLap > 0 ? (Math.ceil(json.FuelPerLap * 10) / 10).toFixed(1) : "-");
 	
-	$("#lap").html((json.NumberOfLaps > 0 && json.CompletedLaps >= json.NumberOfLaps ? json.NumberOfLaps : json.CompletedLaps + 1) + (json.NumberOfLaps > 0 ? "/" + json.NumberOfLaps : ""));
-	$("#pos").html(json.Position + "/" + json.NumCars);
+	lap.html((json.NumberOfLaps > 0 && json.CompletedLaps >= json.NumberOfLaps ? json.NumberOfLaps : json.CompletedLaps + 1) + (json.NumberOfLaps > 0 ? "/" + json.NumberOfLaps : ""));
+	pos.html(json.Position + "/" + json.NumCars);
 	
 	// DRS
-	var drs = $("#drs");
 	var drsText = '-';
 	if (json.DrsEquipped == 1) {
 		if (json.DrsEngaged > 0) {
@@ -129,7 +240,6 @@ function update(json) {
 	drs.html(drsText);
 	
 	//P2P
-	var p2p = $("#p2p");
 	var p2pText = '-';
 	var p2pStyle = '';
 	if (json.PushToPassEquipped == 1) {
@@ -150,22 +260,22 @@ function update(json) {
 	p2p.attr('class', p2pStyle);
 	
 	// Times.
-	$("#laptime").html(formatLapTime(json.LapTimeCurrentSelf));
-	$("#lastlap").html(formatLapTime(json.LapTimePreviousSelf));
-	$("#bestlap").html(formatLapTime(json.LapTimeBestSelf));
-	$("#sessiontime").html(formatTime(json.SessionTimeRemaining));
-	$("#clock").html(moment().local().format("HH:mm:ss"));
+	lapTime.html(formatLapTime(json.LapTimeCurrentSelf));
+	lastLap.html(formatLapTime(json.LapTimePreviousSelf));
+	bestLap.html(formatLapTime(json.LapTimeBestSelf));
+	sessionTime.html(json.SessionTimeRemaining > 0 ? formatTime(json.SessionTimeRemaining) : getSessionName(json.Session));
+	clock.html(moment().local().format("HH:mm:ss"));
 	
 	//Delta
 	if (json.LapTimeCurrentSelf > 0 && json.LapTimeBestSelf > 0) {
-		var delta = json.DeltaBestSelf;
+		var deltaTime = json.DeltaBestSelf;
 		
-		$("#delta").html((delta >= 0 ? '+' : '') + delta.toFixed(3));
-		$("#delta").toggleClass('positive', delta >= 0);
-		$("#delta").toggleClass('negative', delta < 0);
+		delta.html((deltaTime >= 0 ? '+' : '') + deltaTime.toFixed(3));
+		delta.toggleClass('positive', deltaTime >= 0);
+		delta.toggleClass('negative', deltaTime < 0);
 	} else {
-		$("#delta").html('-');
-		$("#delta").removeClass();
+		delta.html('-');
+		delta.removeClass();
 	}
 	
 	// Flags.
@@ -188,31 +298,51 @@ function update(json) {
 		flagClass = 'penalty blink';
 	}
 	
-	var flag = $('#flag');
 	if (!flag.hasClass(flagClass)) {
 		flag.attr('class', flagClass);
 	}
 	
 	// Tires.
-	$('#tire_temp_front_left').html(json.TireTempFrontLeft > -1 ? Math.floor(json.TireTempFrontLeft) + '°C' : 'N/A');
-	$('#tire_temp_front_right').html(json.TireTempFrontRight > -1 ? Math.floor(json.TireTempFrontRight) + '°C' : 'N/A');
-	$('#tire_temp_rear_left').html(json.TireTempRearLeft > -1 ? Math.floor(json.TireTempRearLeft) + '°C' : 'N/A');
-	$('#tire_temp_rear_right').html(json.TireTempRearRight > -1 ? Math.floor(json.TireTempRearRight) + '°C' : 'N/A');
+	tireTempFL.html(json.TireTempFrontLeft > -1 ? Math.floor(json.TireTempFrontLeft) + '°C' : 'N/A');
+	tireTempFR.html(json.TireTempFrontRight > -1 ? Math.floor(json.TireTempFrontRight) + '°C' : 'N/A');
+	tireTempRL.html(json.TireTempRearLeft > -1 ? Math.floor(json.TireTempRearLeft) + '°C' : 'N/A');
+	tireTempRR.html(json.TireTempRearRight > -1 ? Math.floor(json.TireTempRearRight) + '°C' : 'N/A');
 	
-	$('#tire_wear_front_left').html(json.TireWearFrontLeft > -1 ? Math.floor(json.TireWearFrontLeft * 100.0) + '%' : 'N/A');
-	$('#tire_wear_front_right').html(json.TireWearFrontRight > -1 ? Math.floor(json.TireWearFrontRight * 100.0) + '%' : 'N/A');
-	$('#tire_wear_rear_left').html(json.TireWearRearLeft > -1 ? Math.floor(json.TireWearRearLeft * 100.0) + '%' : 'N/A');
-	$('#tire_wear_rear_right').html(json.TireWearRearRight > -1 ? Math.floor(json.TireWearRearRight * 100.0) + '%' : 'N/A');
+	tireWearFL.html(json.TireWearFrontLeft > -1 ? Math.floor(json.TireWearFrontLeft * 100.0) + '%' : 'N/A');
+	tireWearFR.html(json.TireWearFrontRight > -1 ? Math.floor(json.TireWearFrontRight * 100.0) + '%' : 'N/A');
+	tireWearRL.html(json.TireWearRearLeft > -1 ? Math.floor(json.TireWearRearLeft * 100.0) + '%' : 'N/A');
+	tireWearRR.html(json.TireWearRearRight > -1 ? Math.floor(json.TireWearRearRight * 100.0) + '%' : 'N/A');
 	
-	$('#tire_pressure_front_left').html(json.TirePressureFrontLeft > -1 ? Math.floor(json.TirePressureFrontLeft) + 'psi' : 'N/A');
-	$('#tire_pressure_front_right').html(json.TirePressureFrontRight > -1 ? Math.floor(json.TirePressureFrontRight) + 'psi' : 'N/A');
-	$('#tire_pressure_rear_left').html(json.TirePressureRearLeft > -1 ? Math.floor(json.TirePressureRearLeft) + 'psi' : 'N/A');
-	$('#tire_pressure_rear_right').html(json.TirePressureRearRight > -1 ? Math.floor(json.TirePressureRearRight) + 'psi' : 'N/A');
+	tirePressureFL.html(json.TirePressureFrontLeft > -1 ? Math.floor(json.TirePressureFrontLeft) + 'psi' : 'N/A');
+	tirePressureFR.html(json.TirePressureFrontRight > -1 ? Math.floor(json.TirePressureFrontRight) + 'psi' : 'N/A');
+	tirePressureRL.html(json.TirePressureRearLeft > -1 ? Math.floor(json.TirePressureRearLeft) + 'psi' : 'N/A');
+	tirePressureRR.html(json.TirePressureRearRight > -1 ? Math.floor(json.TirePressureRearRight) + 'psi' : 'N/A');
 	
 	updateTireWear('tire_front_left', json.TireWearFrontLeft, json.TireDirtFrontLeft);
 	updateTireWear('tire_front_right', json.TireWearFrontRight, json.TireDirtFrontRight);
 	updateTireWear('tire_rear_left', json.TireWearRearLeft, json.TireDirtRearLeft);
 	updateTireWear('tire_rear_right', json.TireWearRearRight, json.TireDirtRearRight);
+	
+	// Pit screen.
+	pitLimiter.toggleClass('active', json.PitLimiter > 0);
+	if (json.PitLimiter == 1 || json.InPitLane == 1 || json.LastTimeInPit + 5000 > json.LastTimeOnTrack) {
+		if (!pitScreen.is(':visible')) {
+			switchScreen(true);
+		}
+		
+		if (json.LastTimeOnTrack < json.LastTimeInPit) {
+			timeInPitLane.html(((json.LastTimeInPit - json.LastTimeOnTrack) / 1000).toFixed(1));
+		}
+	} else {
+		// Only call once.
+		if (pitScreen.is(':visible')) {
+			switchScreen(false);
+		}
+		
+		timeInPitLane.html('-');
+	}
+	
+	pitLimiter.toggleClass('blink', json.PitLimiter == 1 && json.InPitLane == 0);
 }
 
 function updateTireWear(id, wear, dirt) {
@@ -235,6 +365,18 @@ function formatTime(sec) {
 	return sec < 0 ? '-' : moment.utc(sec * 1000).format("HH:mm:ss");
 }
 
+function getSessionName(index) {
+	if (index == 0) {
+		return 'Practice';
+	} else if (index == 1) {
+		return 'Qualifying';
+	} else if (index == 2) {
+		return 'Race';
+	}
+	
+	return '-';
+}
+
 function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -246,22 +388,5 @@ function convertRpmToPercent(rpm_percent) {
 function updateMaxRpm(maxrpm_tmp, override) {
 	if (maxrpm_tmp > MAXRPM || override) {
 		MAXRPM = maxrpm_tmp;
-	}
-}
-
-function createRpmBarMarkers() {
-	var rpmBar = $("#rpm_bar");
-	
-	// Remove old grid lines.
-	while (rpmBar.firstChild) {
-		rpmBar.removeChild(rpmBar.firstChild);
-	}
-	
-	// Add new grid lines.
-	var percentages = Array(15, 25, 50, 70, 80, 90, 95, 98);
-	for (var i = 0; i < percentages.length; i++) {
-		var line = document.createElement("div");
-		line.style.left = convertRpmToPercent(percentages[i] / 100).toFixed(2) + "%";
-		rpmBar.append(line); 
 	}
 }
